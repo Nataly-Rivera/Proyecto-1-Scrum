db9d51358ab7fce671408b6bb6dba49afc7d7f6e
Alejandra123458 <126327639+Alejandra123458@users.noreply.github.com>
2024-04-06 11:35:46 -0500

    Add files via upload

---
+++ Ejemplo4Api/Ejemplo4Api/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Ejemplo4Api.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+    public DbSet<Usuario> Usuario { get; set; }
+}
---
+++ Ejemplo4Api/Ejemplo4Api/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace Ejemplo4Api.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required int age { get; set; }
+    }
+}
